95cf4608919c8aaef85411283e69cccf082dc77d
Artem Klashtorny <aklashto@uwaterloo.ca>
2021-05-09 22:10:01 -0400

    Added meson build files

---
+++ src/finesse.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define PROJECT_NAME "fiNESse"
+
+int main(int argc, char **argv) {
+    if(argc != 1) {
+        std::cout << argv[0] <<  "takes no arguments." << std::endl;
+        return 1;
+    }
+    std::cout << "This is project " << PROJECT_NAME << "." << std::endl;
+    return 0;
+}
