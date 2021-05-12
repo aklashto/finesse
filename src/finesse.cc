8894c77834e51ba6dc7562afc981e508d6e6f7c3
Artem Klashtorny <aklashto@uwaterloo.ca>
2021-05-11 23:11:19 -0400

    Replaced console prints with spdlog calls

--- src/finesse.cc
+++ src/finesse.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
 #include "nes.hh"
 
 inline bool ends_with(const std::string & input, const std::string & suffix) {
@@ -7,27 +8,27 @@ inline bool ends_with(const std::string & input, const std::string & suffix) {
     if (suffix_length > input_length) {
         return false;
     }
-    return !(input.compare(input_length - suffix_length, suffix_length, suffix));
+    return !(input.compare(input_length - suffix_length,
+                           suffix_length, suffix));
 }
 
 int main(int argc, char **argv) {
     if(argc != 2) {
-        std::cerr << argv[0] <<  " takes an input file as an argument.\n"
-            "Usage: " << argv[0] << " <filename>.nes" << std::endl;
+        spdlog::error("{} takes an input file as argument.", argv[0]);
+        spdlog::error("  Usage: {} <filename>.nes", argv[0]);
         return -1;
     }
 
     std::string input_name(argv[1]);
     if (!ends_with(input_name, ".nes")) {
-        std::cerr << "First argument must be a file name with a .nes extension"
-                  << std::endl;
+        spdlog::error("Argument must be a file name with a .nes extension");
         return -2;
     }
 
     std::ifstream infile(input_name, std::ios::in | std::ios::binary);
 
     if (!infile.is_open()) {
-        std::cerr << "Failed to open test file" << std::endl;
+        spdlog::error("Failed to open test file {}", input_name);
         return -3;
     }
 
--- src/nes.cc
+++ src/nes.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "nes.hh"
+#include <spdlog/spdlog.h>
 
 Nes::Nes() {}
 Nes::~Nes() {}
 void Nes::ReadInputFile(std::ifstream & infile) {
     if (infile.peek() == std::ifstream::traits_type::eof()) return;
-    std::cout << "Non-empty .nes file provided!" << std::endl;
+    spdlog::info("Non-empty .nes file provided!");
 
     char c;
     for (;;) {
