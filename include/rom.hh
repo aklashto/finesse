7ee2a03cd2b671946ff9719c1d6be6f17bc6cc2a
Artem Klashtorny <aklashto@uwaterloo.ca>
2021-05-12 00:00:51 -0400

    Started header file for ROM class

---
+++ include/rom.hh
@@ -0,0 +1,21 @@
+#ifndef ROM_HH
+#define ROM_HH
+
+/**
+ * Container class for the input ROM, which holds program and data for NES
+ * games as well as metadata information about each game and their respective
+ * mappers
+ */
+class Rom {
+ private:
+  // PRG (program) ROM size, in units of 16 KB
+  uint8_t prg_size_;
+  // CHR (character) ROM size, in units of 8 KB
+  uint8_t chr_size_;
+
+ public:
+  Rom(uint8_t prg_size, uint8_t chr_size);
+  ~Rom();
+};
+
+#endif
